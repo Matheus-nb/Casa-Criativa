e9bd12174086ac8600e94be99936ea965b808477
Matheus <Matheus-nb@users.noreply.github.com>
2020-03-27 20:39:41 -0300

    Tentando

---
+++ public/scripts.js
@@ -0,0 +1,47 @@
+function onOff(){
+    document 
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+
+}
+
+function checkFields(event) {
+
+    const ValuesToCheck = [
+        "title",
+        "image",
+        "category",
+        "description",
+        "link",
+    ]
+
+    const isEmpty = ValuesToCheck.find(function(value) {
+
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIfIsEmpty = !event.target[value].value.trim()
+
+        if (checkIfIsString && checkIfIsEmpty) {
+            return true
+        }
+    })
+
+    if(isEmpty) {
+        event.preventDefault()
+        alert("Por favor, preencha todos os campos")
+    }
+
+}
+
+
+
